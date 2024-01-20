239937bdee8331282fccb1e63057a482219918dc
shakthi <shakthikumar123456789@gmail.com>
2024-01-20 18:45:29 +0530

    Create font dropdown menu component

---
+++ src/components/fontDropDown.tsx
@@ -0,0 +1,20 @@
+import { ChangeEvent } from "react";
+import { useFont } from "../state";
+
+export default function FontDropDown() {
+  const { fonts, setFont } = useFont();
+
+  const handleSelection = (e: ChangeEvent<HTMLSelectElement>) => {
+    setFont(e.target.value);
+  };
+
+  return (
+    <select className="font-dropdown" onChange={handleSelection}>
+      {fonts.map((font) => (
+        <option key={font} value={font}>
+          {font}
+        </option>
+      ))}
+    </select>
+  );
+}

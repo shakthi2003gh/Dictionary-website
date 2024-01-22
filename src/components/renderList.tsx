15eea744e40ea48414fba9f703abcb1bf5cea8fc
shakthi <shakthikumar123456789@gmail.com>
2024-01-22 15:01:09 +0530

    Deploy config

--- src/components/renderList.tsx
+++ src/components/renderList.tsx
@@ -1,41 +1,40 @@
 import { useSearch } from "../state";
 import { definitionType } from "../state/types";
 
-type DataType = definitionType[] | string[];
+type DataType = string | definitionType;
 
 type PropType = {
   heading: string;
-  data: DataType;
+  data: DataType[];
 };
 
 export default function List({ heading, data }: PropType) {
   const { handleSearch } = useSearch();
+  const filterData = data.filter((_: DataType, i: number) => i < 10);
 
   return (
     <div className={"list " + heading}>
       <h3>{heading}</h3>
 
       <ul>
-        {data
-          .filter((_, i) => i < 10)
-          .map((value, i) => {
-            if (typeof value === "string")
-              return (
-                <li key={"-" + i}>
-                  <button onClick={() => handleSearch(value)}>{value}</button>
-                </li>
-              );
-
-            const { definition, example } = value;
-
+        {filterData.map((value: DataType, i: number) => {
+          if (typeof value === "string")
             return (
               <li key={"-" + i}>
-                <p className="definition">{definition}</p>
-
-                {example && <q className="example">"{example}"</q>}
+                <button onClick={() => handleSearch(value)}>{value}</button>
               </li>
             );
-          })}
+
+          const { definition, example } = value;
+
+          return (
+            <li key={"-" + i}>
+              <p className="definition">{definition}</p>
+
+              {example && <q className="example">"{example}"</q>}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
